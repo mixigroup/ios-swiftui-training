47231a1ed4eb89d9771dfc21d81ac80f8bd3883f
rockname <8536870+rockname@users.noreply.github.com>
2021-04-21 14:45:40 +0900

    Add session 1.5

--- GitHubClient/Views/RepoListView.swift
+++ GitHubClient/Views/RepoListView.swift
@@ -1,19 +1,33 @@
 import SwiftUI
 
 struct RepoListView: View {
-    private let mockRepos: [Repo] = [
-        .mock1, .mock2, .mock3, .mock4, .mock5
-    ]
+    @State private var mockRepos: [Repo] = []
 
     var body: some View {
         NavigationView {
-            List(mockRepos) { repo in
-                NavigationLink(
-                    destination: RepoDetailView(repo: repo)) {
-                    RepoRow(repo: repo)
+            if mockRepos.isEmpty {
+                ProgressView("loading...")
+            } else {
+                List(mockRepos) { repo in
+                    NavigationLink(
+                        destination: RepoDetailView(repo: repo)) {
+                        RepoRow(repo: repo)
+                    }
                 }
+                .navigationTitle("Repositories")
             }
-            .navigationTitle("Repositories")
+        }
+        .onAppear {
+            loadRepos()
+        }
+    }
+
+    private func loadRepos() {
+        // 1秒後にモックデータを読み込む
+        DispatchQueue.main.asyncAfter(deadline: .now() + 1.0) {
+            mockRepos = [
+                .mock1, .mock2, .mock3, .mock4, .mock5
+            ]
         }
     }
 }
