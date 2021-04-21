93f94744fb267f0de1a8e878e630b9b164d38e26
rockname <8536870+rockname@users.noreply.github.com>
2021-04-21 14:36:18 +0900

    Add session 1.1

--- GitHubClient/ContentView.swift
+++ GitHubClient/ContentView.swift
@@ -2,8 +2,18 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        VStack(alignment: .leading) {
+            Text("Hello, world!")
+                .font(.title)
+                .fontWeight(.bold)
+                .foregroundColor(Color.gray)
+                .padding()
+            Text("Good evening, world!")
+                .font(.body)
+                .fontWeight(.semibold)
+                .foregroundColor(Color.black)
+                .padding()
+        }
     }
 }
 
