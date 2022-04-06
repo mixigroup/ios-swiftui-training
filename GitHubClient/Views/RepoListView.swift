aa2e7bbbebcce817d3a03680465ddd3cb58370d3
Takuya Iitsuka <josesaezu28@gmail.com>
2022-04-07 07:46:30 +0900

    use async/await

--- GitHubClient/Views/RepoListView.swift
+++ GitHubClient/Views/RepoListView.swift
@@ -2,26 +2,14 @@ import SwiftUI
 import Combine
 
 class ReposLoader: ObservableObject {
-    @Published private(set) var repos = [Repo]()
+    @MainActor @Published private(set) var repos = [Repo]()
 
-    private var cancellables = Set<AnyCancellable>()
+    func call() async {
+        try! await Task.sleep(nanoseconds: 1_000_000_000)
 
-    func call() {
-        let reposPublisher = Future<[Repo], Error> { promise in
-            DispatchQueue.global().asyncAfter(deadline: .now() + 1.0) {
-                promise(.success([
-                    .mock1, .mock2, .mock3, .mock4, .mock5
-                ]))
-            }
+        await MainActor.run {
+            repos = [.mock1, .mock2, .mock3, .mock4, .mock5]
         }
-        reposPublisher
-            .receive(on: DispatchQueue.main)
-            .sink(receiveCompletion: { completion in
-                print("Finished: \(completion)")
-            }, receiveValue: { [weak self] repos in
-                self?.repos = repos
-            }
-            ).store(in: &cancellables)
     }
 }
 
@@ -43,7 +31,9 @@ struct RepoListView: View {
             }
         }
         .onAppear {
-            reposLoader.call()
+            Task {
+                await reposLoader.call()
+            }
         }
     }
 }
