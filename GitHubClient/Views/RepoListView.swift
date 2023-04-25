7ae890259e0c2186af0ace55d902afaf642c7131
Takuya Iitsuka <josesaezu28@gmail.com>
2023-04-26 04:28:22 +0900

    GitHubAPIの仕様変更に追従する＆リクエスト数制限回避のためのキャッシュ設定をする

--- GitHubClient/Views/RepoListView.swift
+++ GitHubClient/Views/RepoListView.swift
@@ -10,16 +10,16 @@ class ReposStore: ObservableObject {
         var urlRequest = URLRequest(url: url)
         urlRequest.httpMethod = "GET"
         urlRequest.allHTTPHeaderFields = [
-            "Accept": "application/vnd.github.v3+json"
+            "Accept": "application/vnd.github+json"
         ]
+        // GitHub API のリクエスト数制限(60回/h)回避のためのキャッシュ設定
+        urlRequest.cachePolicy = .returnCacheDataElseLoad
 
         let (data, _) = try! await URLSession.shared.data(for: urlRequest)
 
         let decoder = JSONDecoder()
         decoder.keyDecodingStrategy = .convertFromSnakeCase
-        let value = try! decoder.decode([Repo].self, from: data)
-
-        repos = value
+        repos = try! decoder.decode([Repo].self, from: data)
     }
 }
 
