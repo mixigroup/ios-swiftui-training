5e54b992039c4cef08c4d5b77a6ed1c2f8611580
struuuuggle <mikiya.abe@mixi.co.jp>
2024-03-29 13:45:47 +0900

    refactor: NavigationStack over NavigationView

--- GitHubClient/Models/Repo.swift
+++ GitHubClient/Models/Repo.swift
@@ -1,6 +1,6 @@
 import Foundation
 
-struct Repo: Identifiable, Decodable, Equatable {
+struct Repo: Identifiable, Decodable, Equatable, Hashable {
     var id: Int
     var name: String
     var owner: User
--- GitHubClient/Models/User.swift
+++ GitHubClient/Models/User.swift
@@ -1,6 +1,6 @@
 import Foundation
 
-struct User: Decodable, Equatable {
+struct User: Decodable, Equatable, Hashable {
     var name: String
 
     private enum CodingKeys: String, CodingKey {
