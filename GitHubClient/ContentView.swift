bfca962a0cfc5f4d0007826fe85b9bbf1beb6926
Takuya Iitsuka <josesaezu28@gmail.com>
2024-04-12 15:25:59 +0900

    replace with #Preview

--- GitHubClient/ContentView.swift
+++ GitHubClient/ContentView.swift
@@ -20,8 +20,6 @@ struct ContentView: View {
     }
 }
 
-struct ContentView_Previews: PreviewProvider {
-    static var previews: some View {
-        ContentView()
-    }
+#Preview {
+    ContentView()
 }
