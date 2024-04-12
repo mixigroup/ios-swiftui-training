0c9986e405eac7c1d61c8ecc4fc57c5eab759170
Takuya Iitsuka <josesaezu28@gmail.com>
2024-04-12 14:25:22 +0900

    replace with @Observable macro

--- GitHubClient/Views/RepoListView.swift
+++ GitHubClient/Views/RepoListView.swift
@@ -1,8 +1,10 @@
 import SwiftUI
+import Observation
 
 @MainActor
-class ReposStore: ObservableObject {
-    @Published private(set) var repos = [Repo]()
+@Observable
+class ReposStore {
+    private(set) var repos = [Repo]()
 
     func loadRepos() async {
         let url = URL(string: "https://api.github.com/orgs/mixigroup/repos")!
@@ -15,16 +17,20 @@ class ReposStore: ObservableObject {
         // GitHub API のリクエスト数制限(60回/h)回避のためのキャッシュ設定
         urlRequest.cachePolicy = .returnCacheDataElseLoad
 
-        let (data, _) = try! await URLSession.shared.data(for: urlRequest)
+        let (data, _) = try! await send(urlRequest: urlRequest)
 
         let decoder = JSONDecoder()
         decoder.keyDecodingStrategy = .convertFromSnakeCase
         repos = try! decoder.decode([Repo].self, from: data)
     }
+
+    private nonisolated func send(urlRequest: URLRequest) async throws -> (Data, URLResponse) {
+        try await URLSession.shared.data(for: urlRequest)
+    }
 }
 
 struct RepoListView: View {
-    @StateObject private var reposStore = ReposStore()
+    @State var reposStore: ReposStore
 
     var body: some View {
         NavigationView {
@@ -45,8 +51,6 @@ struct RepoListView: View {
     }
 }
 
-struct RepoListView_Previews: PreviewProvider {
-    static var previews: some View {
-        RepoListView()
-    }
+#Preview {
+    RepoListView(reposStore: ReposStore())
 }
