a43cd85389796e3ef2691663e80920107bc32f93
struuuuggle <mikiya.abe@mixi.co.jp>
2024-04-21 22:23:41 +0900

    feat: MVVMという概念を使わないコードに変更
    see also: e8326322393d9aebc6ddd93017e0da3b8bd1e12b

---
+++ GitHubClient/Stores/ReposStore.swift
@@ -0,0 +1,32 @@
+import Observation
+
+@MainActor
+@Observable
+final class ReposStore {
+    enum Action {
+        case onAppear
+        case onRetryButtonTapped
+    }
+
+    private(set) var state: Stateful<[Repo]> = .loading
+
+    private let repoAPIClient: RepoAPIClientProtocol
+
+    init(repoAPIClient: RepoAPIClientProtocol = RepoAPIClient()) {
+        self.repoAPIClient = repoAPIClient
+    }
+
+    func send(_ action: Action) async {
+        switch action {
+        case .onAppear, .onRetryButtonTapped:
+            state = .loading
+
+            do {
+                let repos = try await repoAPIClient.getRepos()
+                state = .loaded(repos)
+            } catch {
+                state = .failed(error)
+            }
+        }
+    }
+}
